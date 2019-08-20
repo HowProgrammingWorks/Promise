57f7ef9adce0ac749e406bcf2309ae52792c822f
tshemsedinov <timur.shemsedinov@gmail.com>
2019-08-20 13:58:08 +0300

    Implement Expirable as a Promise wrapper

--- JavaScript/b-expirable.js
+++ JavaScript/b-expirable.js
@@ -2,39 +2,68 @@
 
 const PROMISE_TIMEOUT = 1000;
 
-class Expirable extends Promise {
+class Expirable {
   constructor(executor) {
-    super((resolve, reject) => {
+    const promise = new Promise((resolve, reject) => {
       executor(val => {
-        if (this.expired) {
-          reject(new Error('Expired'));
-          return;
-        }
+        if (this.expired) return;
         clearTimeout(this.timer);
         resolve(val);
-      }, reject);
+      }, err => {
+        if (this.expired) return;
+        clearTimeout(this.timer);
+        reject(err);
+      });
+      this.timer = setTimeout(() => {
+        this.expired = true;
+        reject(new Error('Expired'));
+      }, PROMISE_TIMEOUT);
     });
+    this.promise = promise;
     this.expired = false;
-    this.timer = setTimeout(() => {
-      this.expired = true;
-    }, PROMISE_TIMEOUT);
+    this.timer = null;
+    return this.promise;
   }
 }
 
 // Usage
 
 new Expirable(resolve => {
-  setTimeout(resolve, 100);
+  setTimeout(() => {
+    resolve('Resolved before timeout');
+  }, 100);
 }).then(data => {
   console.dir({ data });
 }).catch(error => {
-  console.dir({ error });
+  console.dir({ error: error.message });
+});
+
+new Expirable((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Something went wrong'));
+  }, 100);
+}).then(data => {
+  console.dir({ data });
+}).catch(error => {
+  console.dir({ error: error.message });
 });
 
 new Expirable(resolve => {
-  setTimeout(resolve, 2000);
+  setTimeout(() => {
+    resolve('Never resolved before timeout');
+  }, 2000);
 }).then(data => {
   console.dir({ data });
 }).catch(error => {
-  console.dir({ error });
+  console.dir({ error: error.message });
+});
+
+new Expirable((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Never rejected before timeout'));
+  }, 2000);
+}).then(data => {
+  console.dir({ data });
+}).catch(error => {
+  console.dir({ error: error.message });
 });
