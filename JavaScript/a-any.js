88b9b1c574281b5ff3edf3214f55b2196b3d298f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-02-19 01:58:26 +0200

    Add `Promise.prototype.any` example

---
+++ JavaScript/a-any.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const baseUrl = 'http://localhost:3000';
+
+const promises = [
+  fetch(baseUrl + '/person'),
+  fetch(baseUrl + '/'),
+  fetch(baseUrl + '/city'),
+];
+
+Promise.any(promises)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
